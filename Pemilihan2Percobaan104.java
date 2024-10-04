28944362ecfd1e89db8758db097a3d09fd95b7f7
bungaauulia <bungaaulia1215@gmail.com>
2024-10-04 14:47:30 +0700

    commit percobaan 1

---
+++ Pemilihan2Percobaan104.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Pemilihan2Percobaan104 {
+
+    public static void main(String[] args) {
+        Scanner Absen = new Scanner(System.in);
+
+        int tahun;
+
+        System.out.print("Masukkan tahun: ");
+        tahun = Absen.nextInt();
+
+        if ((tahun % 4) == 0){
+            if ((tahun % 100) != 0)
+                System.out.println("Tahun Kabisat");
+
+        } else 
+            System.out.println("Bukan Tahun Kabisat");
+    }}
+
+                
